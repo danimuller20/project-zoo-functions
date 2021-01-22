27d90c061ea54f491446c9a35b70e75134ae2cba
BernardoFontes <bfontes.1997@gmail.com>
2021-01-22 16:30:42 -0300

    a

--- src/zoo.js
+++ src/zoo.js
@@ -8,42 +8,42 @@ eslint no-unused-vars: [
   }
 ]
 */
-const { animals, employees, prices, hours } = require('./data');
+const { animals, employees, prices } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  const animal = data.animals.filter((bicho) => ids.includes(bicho.id));
+  const animal = data.animals.filter(bicho => ids.includes(bicho.id));
   return animal;
 }
 
-function animalsOlderThan(especie,idade) {
- const animaisFiltrados = data.animals.find((bicho) => bicho.name === especie);
- if (animaisFiltrados.residents.every((animal) => animal.age > idade)){
-   return true;
- } else {
-   return false;
- }
+function animalsOlderThan(especie, idade) {
+   const animaisFiltrados = data.animals.find(bicho => bicho.name === especie);
+   if (animaisFiltrados.residents.every((animal) => animal.age > idade)) {
+    return true;
+  }
+    return false;
 }
 
 console.log(animalsOlderThan('lions', 53));
 
 function employeeByName(employeeName) {
-  if (employeeName === null){
+  if (employeeName === null) {
     return {};
   }
-  let funcionarios = data.employees.find((func) => func.firstName === employeeName || func.lastName === employeeName);
+  const funcionarios = data.employees.find(func => 
+  func.firstName === employeeName || func.lastName === employeeName);
   return funcionarios;
 }
 console.log(employeeByName('Ola'));
 
 function createEmployee(personalInfo, associatedWith) {
-  const novoFuncionario = {...personalInfo, ...associatedWith};
+  const novoFuncionario = { ...personalInfo, ...associatedWith };
   return novoFuncionario;
 }
 console.log(createEmployee());
 
 function isManager(id) {
-  const gerente = employees.some((elemento) => elemento.managers.includes(id));
+  const gerente = employees.some(elemento => elemento.managers.includes(id));
   return gerente;
 }
 
@@ -55,7 +55,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
     managers,
     responsibleFor,
   };
-return data.employees.push(newFunc);
+  return data.employees.push(newFunc);
 }
 
 function animalCount(species) {
@@ -65,18 +65,18 @@ function animalCount(species) {
       return acc;
     }, {});
   }
- return data.animals.find((animal) => (animal.name === species).residents.length);
-  }
+  return data.animals.find(animal => (animal.name === species).residents.length);
+}
 
 function entryCalculator(entrants) {
   if (typeof (entrants) !== 'object') { return 0; }
- entrants = {
-    Adult: 0,
-    Child: 0,
-    Senior: 0
- }
- const valor = (prices.Adult * Adult) + (prices.Child * Child) + (prices.Senior * Senior);
- return valor;
+   entrants = {
+   Adult: 0,
+   Child: 0,
+   Senior: 0,
+  };
+   const valor = (prices.Adult * Adult) + (prices.Child * Child) + (prices.Senior * Senior);
+   return valor;
 }
 
 function animalMap(options) {
@@ -88,11 +88,11 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
- const findFunc = data.employees.find((funcionario) => (funcionario.id === id));
- const findAnimal = data.animals.find(animal => animal.id === findFunc.responsibleFor[0]);
- const sortAnimal = findAnimal.residents.sort((a, b) => b.age - a.age);
- const { name,sex,age } = sortAnimal[0];
- return [name,sex,age];
+  const findFunc = data.employees.find((funcionario) => (funcionario.id === id));
+  const findAnimal = data.animals.find(animal => animal.id === findFunc.responsibleFor[0]);
+  const sortAnimal = findAnimal.residents.sort((a, b) => b.age - a.age);
+  const { name,sex,age } = sortAnimal[0];
+  return [name,sex,age];
 }
 console.log(oldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
