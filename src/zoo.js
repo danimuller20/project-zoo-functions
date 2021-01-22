49fbd0f3e7b61e7a2e875f61ef88429995fd7bb7
Christopher <chris.tryber@gmail.com>
2021-01-22 16:50:07 -0300

    detalhes CodeClimate

--- src/zoo.js
+++ src/zoo.js
@@ -13,21 +13,20 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  let resposta = [];
+  const resposta = [];
 
-  if (ids.length === 0){
-    return ids
+  if (ids.length === 0) {
+    return ids;
   }
-  for (let index = 0; index < ids.length; index += 1){
-    data.animals.forEach((animal)=> {
+  for (let index = 0; index < ids.length; index += 1) {
+    data.animals.forEach((animal) => {
       if (animal.id === ids[index]){
-        resposta.push(animal)
+        resposta.push(animal);
       }
-    })
+    });
   }
-  return resposta
+  return resposta;
 }
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
