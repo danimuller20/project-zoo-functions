23f13db893a306fe4280857e85b8ce6da171fe43
Renato Feitosa <pythaqua@gmail.com>
2021-01-22 16:12:31 -0300

    [feat] (isManager) add 5th requirement

--- src/zoo.js
+++ src/zoo.js
@@ -31,9 +31,12 @@ const employeeByName = (employeeName) => {
 
 const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...associatedWith });
 
-function isManager(id) {
-  // seu código aqui
-}
+// const getEmployeeById = id => employees.find(employee => employee.id === id);
+
+const isManager = (id) => {
+  const managersIds = employees.reduce((acc, current) => [...acc, ...current.managers], []);
+  return managersIds.includes(id);
+};
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
