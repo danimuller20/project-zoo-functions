808b4b40fc374871c1935169dedb137f87a02cfc
Felipe Rocha <felipe.sr27@gmail.com>
2021-01-22 17:11:45 -0300

    Requirement 1: implemented function animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -10,9 +10,10 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const {animals} = data;
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return animals.filter((animal) => ids.find((id) => id === animal.id));
 }
 
 function animalsOlderThan(animal, age) {
