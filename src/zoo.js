553d55dc2c5f879784811ec2b2e4e5b3386e2545
edison-ps <edison@osite.com.br>
2021-01-22 16:56:30 -0300

    animalsByIds V1.01

--- src/zoo.js
+++ src/zoo.js
@@ -14,12 +14,11 @@ const data = require('./data');
 
 function animalsByIds(...ids) {
   if (ids.length === 0) {
-    return [ ];
-  } else {
-    return ids.map((arrayIds) => {
-      return animals.find((arrayAnimals) => arrayAnimals.id === arrayIds);
-    });
-  }
+    return [];
+  } 
+  return ids.map((arrayIds) => {
+    return animals.find((arrayAnimals) => arrayAnimals.id === arrayIds);
+  });
 }
 
 //console.log('resultado' ,animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
