a32c008c6110d605b94c587d11c1ea81300ec4ca
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-22 16:38:42 -0300

    animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -10,10 +10,20 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = data;
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  const animalsFind = [];
+  ids.forEach((id) => {
+    animalsFind.push(animals.find((animal) => {
+      return animal.id === id
+    }))
+  })
+
+  return animalsFind
 }
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
