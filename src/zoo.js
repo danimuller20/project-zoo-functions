02c6b4e85906995a7336db6b5eb3d9ea338f5234
edison-ps <edison@osite.com.br>
2021-01-22 17:00:28 -0300

    animalsByIds V1.02

--- src/zoo.js
+++ src/zoo.js
@@ -15,13 +15,9 @@ const data = require('./data');
 function animalsByIds(...ids) {
   if (ids.length === 0) {
     return [];
-  } 
-  return ids.map((arrayIds) => {
-    return animals.find((arrayAnimals) => arrayAnimals.id === arrayIds);
-  });
+  }
+  return ids.map((arrayIds) => animals.find((arrayAnimals) => arrayAnimals.id === arrayIds));
 }
-
-//console.log('resultado' ,animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
