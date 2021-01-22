47cccc592bb27c8089d9797539dd385c163c7a98
Renato Feitosa <pythaqua@gmail.com>
2021-01-22 15:20:02 -0300

    [feat] (animalsByIds) implement function

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-}
+const { animals } = data;
+
+const animalsByIds = (...ids) => animals.filter(animal => ids.includes(animal.id));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
