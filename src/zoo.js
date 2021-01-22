8672d23649d361052e46aba8f4d61d2b4757b003
Matheus Martino <matheus.martino97@gmail.com>
2021-01-22 15:59:29 -0300

    Initial commit.

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,13 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+
+  
 }
+
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
