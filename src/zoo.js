e75f5ab35ea95b385251fc35a765df13c08d0b3c
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-22 17:25:24 -0300

    Inserçao de teste

--- src/zoo.js
+++ src/zoo.js
@@ -12,8 +12,10 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  alert('inicio do projeto');
-  // seu código aqui
+  if (ids === undefined) {
+    const emptyArray = [];
+  }
+  return emptyArray;
 }
 
 function animalsOlderThan(animal, age) {
