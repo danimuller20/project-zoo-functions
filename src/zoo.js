cad9434d7d8573089fe8754317ccffb338652531
BBrolesi <brunohsbrolesi@gmail.com>
2021-01-22 18:37:37 -0300

    Correct some code climate issues

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees, hours, prices } = data;
+const { animals, employees, _, prices } = data;
 
 function animalsByIds(...ids) {
   const animalsIds = [...ids];
