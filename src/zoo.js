e78e1869877b06ca1ddb7f6a85604c3ac9716c3d
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-22 14:13:08 -0400

    Corrige CC

--- src/zoo.js
+++ src/zoo.js
@@ -13,7 +13,7 @@ const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
-  return data.animals.filter((animal) => ids.some((id) => animal.id === id));
+  return data.animals.filter(animal => ids.some(id => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
@@ -22,10 +22,10 @@ function animalsOlderThan(animal, age) {
   return especie.residents.every(obj => obj.age > age);
 }
 
-function employeeByName(employeeName) {
+function employeeByName(name) {
   // seu código aqui
-  let employee = data.employees.find((employ) => employ.firstName === employeeName || employ.lastName === employeeName);
-  if (!employeeName) employee = {};
+  let employee = data.employees.find(e => e.firstName === name || employ.lastName === name);
+  if (!name) employee = {};
   return employee;
 }
 
@@ -33,22 +33,22 @@ function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
   const { id, firstName, lastName } = personalInfo;
   const { managers, responsibleFor } = associatedWith;
-  return {id, firstName, lastName, managers, responsibleFor,};
+  return { id, firstName, lastName, managers, responsibleFor, };
 }
 
 function isManager(id) {
   // seu código aqui
-  return data.employees.some((employ) => employ.managers.some((idEmploy) => idEmploy === id));
+  return data.employees.some(employ => employ.managers.some(idEmploy => idEmploy === id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
-  data.employees.push({id, firstName, lastName, managers, responsibleFor,});
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor, });
 }
 
 function createNumberAnimals() {
   return data.animals.reduce((acc, animal) => {
-    acc[animal.name] = animal.residents.length,{};
+    acc[animal.name] = animal.residents.length;
     return acc;
   }, {});
 }
@@ -64,12 +64,11 @@ function animalCount(species) {
 
 function entryCalculator(entrants = {}) {
   // seu código aqui
-  if (Object.keys(entrants).length < 1) return 0
+  if (Object.keys(entrants).length < 1) return 0;
   return Object.keys(entrants).reduce((acc, age) => {
-    acc += data.prices[age] * entrants[age]
+    acc += data.prices[age] * entrants[age];
     return acc;
   }, 0);
-
 }
 
 function animalMap(options) {
