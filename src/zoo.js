224c6e811bc9e31848d6a446d2723a1a95d885af
edison-ps <edison@osite.com.br>
2021-01-22 18:26:14 -0300

    createEmployee V1.00

--- src/zoo.js
+++ src/zoo.js
@@ -32,8 +32,8 @@ function employeeByName(employeeName) {
   employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+function createEmployee({id, firstName, lastName}, {managers, responsibleFor}) {
+  return { id, firstName, lastName, managers, responsibleFor, };
 }
 
 function isManager(id) {
