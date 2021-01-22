313158cf05fd52d4b9fd18e6496e6439df5fbf24
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-22 16:13:01 -0300

    Adiciona requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -22,6 +22,8 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  const species = animals.find((element) => element.name === animal);
+  return species.residents.every((element) => element.age > age)
 }
 
 function employeeByName(employeeName) {
