15beff2d989f7143395b5e844d1edaa3250c63f6
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-01-22 14:04:47 -0400

    Adiciona requisitos do 1 ao 8

--- src/zoo.js
+++ src/zoo.js
@@ -11,36 +11,65 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  return data.animals.filter((animal) => ids.some((id) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  const especie = data.animals.find(specie => specie.name === animal);
+  return especie.residents.every(obj => obj.age > age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  let employee = data.employees.find((employ) => employ.firstName === employeeName || employ.lastName === employeeName);
+  if (!employeeName) employee = {};
+  return employee;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {id, firstName, lastName, managers, responsibleFor,};
 }
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((employ) => employ.managers.some((idEmploy) => idEmploy === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  data.employees.push({id, firstName, lastName, managers, responsibleFor,});
+}
+
+function createNumberAnimals() {
+  return data.animals.reduce((acc, animal) => {
+    acc[animal.name] = animal.residents.length,{};
+    return acc;
+  }, {});
 }
 
 function animalCount(species) {
   // seu código aqui
+  const animalNumber = createNumberAnimals();
+  if (!species) {
+    return animalNumber;
+  }
+  return animalNumber[species];
 }
 
-function entryCalculator(entrants) {
+function entryCalculator(entrants = {}) {
   // seu código aqui
+  if (Object.keys(entrants).length < 1) return 0
+  return Object.keys(entrants).reduce((acc, age) => {
+    acc += data.prices[age] * entrants[age]
+    return acc;
+  }, 0);
+
 }
 
 function animalMap(options) {
