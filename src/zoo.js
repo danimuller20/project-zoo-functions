70264ec5c8d26af92c6e1a61281584634c1144a9
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-01-22 16:57:27 -0300

    Revert "requisito 1 feito"
    This reverts commit 3dc3b819ece517534ff2ed36bdad801a10d25a62.

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,10 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(...ids) {
-  return animals.filter(({ id }, index) => id === ids[index])
+function animalsByIds(ids) {
+  // seu código aqui
 }
 
 function animalsOlderThan(animal, age) {
