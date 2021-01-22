8cff1f897f469dc4eb8c4a924e0ffd2846feacfa
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-22 16:15:34 -0300

    CC

--- src/zoo.js
+++ src/zoo.js
@@ -22,8 +22,12 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  const species = animals.find((element) => element.name === animal);
-  return species.residents.every((element) => element.age > age)
+  const species = animals.find((element) => {
+    element.name === animal
+  });
+  return species.residents.every((element) => {
+    return element.age > age
+  });
 }
 
 function employeeByName(employeeName) {
