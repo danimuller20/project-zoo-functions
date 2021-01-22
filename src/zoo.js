21d2dc7e0d6ac592adcdf5bb6ee8f338cfb7a963
LucioOSilva <lucioosilva00@gmail.com>
2021-01-22 18:25:30 -0300

    functions - sintax adjusted6

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees} = data;
+const { animals, employees } = data;
 
 function animalsByIds(...ids) {
   const animalsFound = [];
