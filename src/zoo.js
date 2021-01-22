21d85f385ec923c269184e8b735e90684c0c5e67
Felipe Cardoso <felipedclc@gmail.com>
2021-01-22 18:21:10 -0300

    code climate

--- src/zoo.js
+++ src/zoo.js
@@ -38,7 +38,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return personalInfo = { ...personalInfo, ...associatedWith };
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
