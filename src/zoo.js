0f01c65ba1d54720d6dd7d1678da354361603b88
Matheus Mendes <matheus.unilog@gmail.com>
2021-01-23 00:55:49 -0300

    Request 6 v2

--- src/zoo.js
+++ src/zoo.js
@@ -44,7 +44,7 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  return employees.push = {
+    employees.push = {
     id,
     firstName,
     lastName,
