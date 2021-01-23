c6a31ce549314cd99ebf98780c113366c0020f32
santiago <felipesantiagolds@gmail.com>
2021-01-23 12:24:18 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -56,8 +56,8 @@ const isManager = (id) => {
   return check;
 };
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
