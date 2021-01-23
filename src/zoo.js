8576427e9e78b997c24f6febb125c07fc77d3e71
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-01-23 19:18:20 -0300

    Acertando requisito 1 e 2

--- src/zoo.js
+++ src/zoo.js
@@ -14,16 +14,15 @@ const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
-  
-  return animals.filter((animalsId, i) => animalsId.id === ids[i])
+  return animals.filter((animalsId, i) => animalsId.id === ids[i]);
 }
 console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  let animalEncontrado = animals.find((myAnimal) => myAnimal.name === animal)
-  return animalEncontrado.residents.every((idade) => idade.age >= age)
-};
+  const animalEncontrado = animals.find(myAnimal => myAnimal.name === animal);
+  return animalEncontrado.residents.every(idade => idade.age >= age);
+}
 
 function employeeByName(employeeName) {
   // seu código aqui
