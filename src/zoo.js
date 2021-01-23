938fd08bfc0f6f4225f4175b438d514eacad9234
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-01-23 08:41:54 -0300

    requisito 6 feito

--- src/zoo.js
+++ src/zoo.js
@@ -34,8 +34,8 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.some((managerid) => managerid === id))
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
