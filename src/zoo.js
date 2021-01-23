63a8c0a91c86e00f40edc0f13eb1cbb4195a5f89
Felipe Rocha <felipe.sr27@gmail.com>
2021-01-23 18:46:01 -0300

    Requirement 6: implemented function addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -36,8 +36,8 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.find(managerId => managerId === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
