81272a0553227f86444308ccc605b0c9f5afe19c
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-01-23 15:46:10 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,18 @@ function isManager(id) {
   return arrayManagers.some(value => value === id);
 }
 
-function addEmployee({ id, firstName, lastName }, { managers, responsibleFor }) {
-  return { id, firstName, lastName, managers, responsibleFor };
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor
+  });
+  console.log(employees);
 }
+
+addEmployee('7ed1c9bb-8570-44f6-b718-0666b869573a','John','Doe',['c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1','9e7d4524-363c-416a-8759-8aa7e50c0992'],['0938aa23-f153-4937-9f88-4858b24d6bce','89be95b3-47e4-4c5b-b687-1fabf2afa274','bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5']);
 
 function animalCount(species) {
   // seu código aqui
