1792369a932686a2ca1602302863bb5624555fe9
Daniela Muller <danielamuller20@gmail.com>
2021-01-23 14:37:11 -0300

    code climate

--- src/zoo.js
+++ src/zoo.js
@@ -48,7 +48,7 @@ function isManager(id) {
 
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  data.employees.push({id, firstName, lastName, managers, responsibleFor});
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
