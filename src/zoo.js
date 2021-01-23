d28e462d484a9476501f531f31ebdd38695c87ab
Kevin Fraga <kevinciolac@gmail.com>
2021-01-22 21:45:02 -0300

    :four: function createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -35,6 +35,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return Object.assign({}, personalInfo, associatedWith);
 }
 
 function isManager(id) {
