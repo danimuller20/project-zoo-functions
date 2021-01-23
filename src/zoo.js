175cad9209f29ab05eb62b5713ced38ae39efe28
Felipe Rocha <felipe.sr27@gmail.com>
2021-01-23 18:28:51 -0300

    Requirement 5: implemented function isManager

--- src/zoo.js
+++ src/zoo.js
@@ -35,7 +35,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.find(managerId => managerId === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
