ef9c3a5b26053f1378c6df85d8ce8fbf76075c1c
Kevin Fraga <kevinciolac@gmail.com>
2021-01-22 22:55:44 -0300

    :six: function addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -41,8 +41,9 @@ function isManager(id) {
   return data.employees.some(person => person.managers.find(tag => tag === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
