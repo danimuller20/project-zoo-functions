e0617bfe29d2f1843b4cf74ba9548aec26feac02
santiago <felipesantiagolds@gmail.com>
2021-01-24 16:26:13 -0300

    Requisito 11 code climate v.0.9

--- src/zoo.js
+++ src/zoo.js
@@ -92,7 +92,8 @@ function schedule(dayName) {
 function oldestFromFirstSpecies(id) {
   return Object.values(animals.find(specie => specie.id === employees
     .filter(numberId => numberId.id === id)[0].responsibleFor[0]).residents
-    .reduce((accumulater, currentAge) => ( accumulater.age > currentAge.age ? accumulater : currentAge
+    .reduce((accumulater, currentAge) => (
+      accumulater.age > currentAge.age ? accumulater : currentAge
     )));
 }
 
