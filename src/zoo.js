5be880dd419a2e113902b8103642a703a7eb0413
Daniela Muller <danielamuller20@gmail.com>
2021-01-24 11:58:39 -0300

    animalCount function

--- src/zoo.js
+++ src/zoo.js
@@ -52,7 +52,13 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
-  // seu código aqui
+  return animals.reduce((accumulator, currentValue) => {
+    if (currentValue.name === species) {
+      return currentValue.residents.length
+    }
+    accumulator[currentValue.name] = currentValue.residents.length;
+    return accumulator;  
+  }, {});
 }
 
 function entryCalculator(entrants) {
