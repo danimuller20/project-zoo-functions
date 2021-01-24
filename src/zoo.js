00743f858b3ec6d71ec11885d6d6b64fbc56ca5a
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-01-24 20:07:43 -0300

    fix codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -132,8 +132,11 @@ function oldestFromFirstSpecies(id) {
   // seu código aqui
   const employee = data.employees.find(value => value.id === id);
   const specie = data.animals.find(value => value.id === employee.responsibleFor[0]);
-  const animalResult = specie.residents.reduce((oldAnimal, currentValue) => 
-    (currentValue.age >= oldAnimal.age) ? currentValue : oldAnimal);
+  const animalResult = specie.residents.reduce((oldAnimal, currentValue) => {
+    if (currentValue.age >= oldAnimal.age) {
+      return currentValue;
+    }
+  });
   return Object.values(animalResult);
 }
 
