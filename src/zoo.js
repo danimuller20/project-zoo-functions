90490ba934a35bba6b8251e7d31b4c8454b8e000
palenske <viniciuspalenske@gmail.com>
2021-01-24 15:38:11 -0300

    addEmployee refatorada

--- src/zoo.js
+++ src/zoo.js
@@ -36,8 +36,7 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  const newEmployee = { ...employees[0] = { id, firstName, lastName, managers, responsibleFor } };
-  return employees.push(newEmployee);
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
