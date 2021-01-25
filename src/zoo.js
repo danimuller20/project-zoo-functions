91696d3eadd3f222d0e458e7846381c762358b26
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-01-25 14:06:10 -0300

    createEmployee OK codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees, prices} = data; // destructure
+const { animals, employees} = data; // destructure
 
 function animalsByIds(...ids) {
   // seu código aqui
