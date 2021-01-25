b1c7fdbd37e1081038d189668bdfba164572b510
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-24 21:23:55 -0300

    Ex. 11 - codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -70,9 +70,9 @@ function schedule(dayName) {
 function oldestFromFirstSpecies(id) {
   const filteredEmployee = employees.find(employee => employee.id === id);
   let animalEmployee = animalsByIds(filteredEmployee.responsibleFor[0]);
-  animalEmployee = animalEmployee[0].residents.reduce((acc, curr) =>
+  const oldestAnimal = animalEmployee[0].residents.reduce((acc, curr) =>
     acc = ((acc.age < curr.age) ? curr : acc));
-  return [animalEmployee.name, animalEmployee.sex, animalEmployee.age];
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 function increasePrices(percentage) {
