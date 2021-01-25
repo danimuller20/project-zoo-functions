861ac56bbadda885a17e7ecdd2f6dc38f966bd79
Hudson Carlos <hc88806@gmail.com>
2021-01-25 10:34:58 -0400

    breaking console.log

--- src/zoo.js
+++ src/zoo.js
@@ -35,7 +35,6 @@ function createEmployee(personalInfo, associatedWith) {
 function isManager(id) {
   return data.employees.find(verifica => verifica.managers === id);
 }
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
