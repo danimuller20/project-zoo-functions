8a9762ce67d21522e93e7aa3e597c43c62748274
lhfmworker <lhfmworker@gmail.com>
2021-01-25 13:25:27 -0300

    entrega oldestFromFirstSpecies codeclimate2

--- src/zoo.js
+++ src/zoo.js
@@ -83,11 +83,11 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  const personFound = data.employees.filter(person => person.id.includes(id)).
-  find(value => value.managers !== undefined).responsibleFor[0];
+  const personFound = data.employees.filter(person => person.id.includes(id))
+  .find(value => value.managers !== undefined).responsibleFor[0];
   const residents = data.animals.find(object => object.id === personFound).residents;
-  const oldest = residents.map(animal => animal.age).
-  reduce((acc, cur) => Math.max(acc, cur));
+  const oldest = residents.map(animal => animal.age)
+  .reduce((acc, cur) => Math.max(acc, cur));
   return Object.values(residents.find(animals => animals.age === oldest));
 }
 
