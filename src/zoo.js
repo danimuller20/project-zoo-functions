b88b0d9215824b2d9e3575c8276cc05e8a763529
Hudson Carlos <hc88806@gmail.com>
2021-01-25 10:33:14 -0400

    add if function employeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -21,17 +21,22 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
+  if (employeeName === undefined){
+    return {};
+  }
   return data.employees.find(nome => nome.firstName === employeeName ||
     nome.lastName === employeeName);
 }
+console.log(employeeByName());
 
 function createEmployee(personalInfo, associatedWith) {
   return Object.assign(personalInfo, associatedWith);
 }
 
 function isManager(id) {
-
+  return data.employees.find(verifica => verifica.managers === id);
 }
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
