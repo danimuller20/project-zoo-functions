c14298cc64ad87fef44a4a7f4301f67b0d3bf28d
rafaelgeronimo <rafaelgeronimo@outlook.com>
2021-01-25 19:24:02 -0300

    Function entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 
 function animalsByIds(...ids) {
   return animals.filter(animal => ids.some(id => animal.id === id));
@@ -50,7 +50,9 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  return ((Adult * prices.Adult) + (Child * prices.Child) + (Senior * prices.Senior));
 }
 
 function animalMap(options) {
