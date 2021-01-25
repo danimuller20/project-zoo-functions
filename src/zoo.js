78673dfeabf1fe4c1d2655d8a5c142208a2f2545
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-25 15:33:52 -0300

    Primeiro Requisito

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,9 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // 
+function animalsByIds(...ids) {
+  const retorna = data.animals.filter(animal => ids.includes(animal.id));
+  return retorna;
 }
 
 function animalsOlderThan(animal, age) {
