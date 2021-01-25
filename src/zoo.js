6f1ca41785b53e67480c3cb106d24d2814033797
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-25 17:25:31 -0300

    Resolução do requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -9,6 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
+const { hours } = require('./data');
 const data = require('./data');
 
 const { animals, employees, prices } = data;
@@ -92,21 +93,22 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // if (dayName === 'Monday') return { Monday: 'CLOSED' };
+  if (dayName === 'Monday') return { Monday: 'CLOSED' };
+  const openingHours = hours[dayName];
 
-  // if (dayName !== undefined) {
-  //   const openingHours = hours[dayName];
-  //   return { [dayName]: `Open from ${openingHours.open}am until ${openingHours.close}pm` };
-  // }
+  if (dayName !== undefined) {
+    return { [dayName]: `Open from ${openingHours.open}am until ${openingHours.close - 12}pm` };
+  }
 
-  // const { Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Monday } = hours;
-  // hours.forEach(day => {
-  //   ({ [dayName]: `Open from ${day.open}am until ${day.close}pm` });
-  // })
-
+  const newObj = {};
+  Object.keys(hours).forEach((day) => {
+    newObj[day] = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`;
+    if (hours[day].open === 0) {
+      newObj[day] = 'CLOSED';
+    }
+  });
+  return newObj;
 }
-// console.log(schedule('Friday'));
-// // 'Tuesday': 'Open from 8am until 6pm'
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
@@ -117,8 +119,38 @@ function increasePrices(percentage) {
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  const employeeResponsible = () => {
+    const newArray = [];
+    employees.forEach(employee => {
+      employee.responsibleFor.forEach(id => {
+        const animalItem = animals.find(animal => animal.id === id);
+        if (!newArray.includes(animalItem)) {
+          newArray.push(animalItem);
+        }
+      })
+    })
+    return newArray;
+  }
+
+  const result = {};
+  const animalResponsibleFor = employee => employeeResponsible()
+    .filter(animal => employee.responsibleFor.includes(animal.id))
+    .map(animal => animal.name);
+
+  if (idOrName === undefined) {
+    employees.forEach((employee) => {
+      result[`${employee.firstName} ${employee.lastName}`] = animalResponsibleFor(employee);
+    });
+    return result;
+  }
+
+  const employeeName = employees
+    .find(employee => employee.id === idOrName
+    || employee.firstName === idOrName || employee.lastName === idOrName);
+  result[`${employeeName.firstName} ${employeeName.lastName}`] = animalResponsibleFor(employeeName);
+  return result;
 }
+// console.log(employeeCoverage());
 
 module.exports = {
   entryCalculator,
