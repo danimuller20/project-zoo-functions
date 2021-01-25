51166d13f05d3d1318aaf9f0e5660075dae6d215
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-01-25 17:09:52 -0300

    adicionando function animalsById

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,8 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return data.animals.filter((value, index) => value.id === ids[index]);
 }
 
 function animalsOlderThan(animal, age) {
