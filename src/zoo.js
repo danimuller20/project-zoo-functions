e525928b93337bd826983f20e9c86b222c616c5d
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-01-24 20:50:34 -0400

    Comitao da massa 2

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees, hours, prices } = data;
+const { animals, employees, _, prices } = data;
 
 function animalsByIds(...ids) {
   return animals.filter(animal => ids.includes(animal.id));
@@ -49,7 +49,7 @@ function animalCount(species) {
   if (!species) {
     const obj = {};
     animals.forEach(animal => {
-      obj[animal.name] = animal.residents.length;
+      obj[animal.name] = animal.residents.length
     });
     return obj;
   }
