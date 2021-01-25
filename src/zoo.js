862ce0fa627d74a56605db130dfc709a5b50cbe9
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-01-25 14:44:26 -0300

    lesson: codeClimate

--- src/zoo.js
+++ src/zoo.js
@@ -12,9 +12,10 @@ const { animals } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  if ( !Array.isArray (ids) || ids.length === 0 )
+  if (!Array.isArray (ids) || ids.length === 0) {
     return [];
-return animals.filter(animal => ids.includes(animal.id));
+  }
+  return animals.filter(animal => ids.includes(animal.id));
 }
 
 
