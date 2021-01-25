4acb1c5e582823ab2de30cb7fcd0ac30e7114c63
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-01-25 11:47:31 -0300

    ?

--- src/zoo.js
+++ src/zoo.js
@@ -47,9 +47,8 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function animalCount(species) {
   // seu código aqui
-  let countReport = animals.reduce((accObject, current) => {
-  accObject[current.name] = current.residents.length;
-    return accObject}, {});
+  let countReport = animals.reduce((accObject, current) => {  accObject[current.name] = current.residents.length;
+    return accObject} , {} );
   if (Object.keys(countReport).includes(species)) {
     countReport = countReport[species];
   }
