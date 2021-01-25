1243eada2d7d492bde377e49f9eeb87fac802297
cleber-teixeira <cleberlt@gmail.com>
2021-01-25 18:16:47 -0300

    Requisito 06 - Implementa a função "addEmployee". :white_check_mark:

--- src/zoo.js
+++ src/zoo.js
@@ -47,8 +47,15 @@ function isManager(id) {
   return result;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
