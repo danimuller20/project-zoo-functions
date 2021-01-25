8b429bf2cf2db4f210306808cf7c53fc08fc28b0
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-01-25 16:06:15 -0300

    requeriment

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { employees, animals } = data;
+const { employees, animals, prices } = data;
 
 function animalsByIds(...ids) {
   if (ids === undefined) {
@@ -76,11 +76,18 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstResponsibleFor = employees.find(employee => employee.id === id).responsibleFor[0];
+  const caredAnimals = animals.find(animal => animal.id === firstResponsibleFor).residents;
+  const oldest = caredAnimals.sort((animal1, animal2) => animal2.age - animal1.age)[0];
+  const { name, sex, age } = oldest;
+  return [name, sex, age];
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const pricesKeys = Object.keys(prices);
+  pricesKeys.forEach((price) => {
+    prices[price] = Math.round(prices[price] * (1 + (percentage / 100)) * 100) / 100;
+  });
 }
 
 function employeeCoverage(idOrName) {
