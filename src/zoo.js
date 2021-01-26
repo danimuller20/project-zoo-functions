2f24861f51bf6163e1554474cb553853909cb3ff
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-01-26 18:52:18 -0300

    4 ja foram faltam 9

--- src/zoo.js
+++ src/zoo.js
@@ -9,25 +9,68 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals, employees } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const checkVar = varChecked => {
+  if(!varChecked) {
+		return undefined;
+	}
+}
+
+const checkObj = objectChecked => {
+  if (!objectChecked) {
+    return {};
+  }
+}
+
+const checkTwoVar = (varChecked1, varChecked2) => {
+  if(varChecked1) {
+    return varChecked1;
+  }
+  return varChecked2
+}
+
+function animalsByIds(...ids) {
+  checkVar(ids);
+  return data.animals.filter(animal => ids.find(id => animal.id == id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return data
+    .animals.find(typeName => typeName.name === animal)
+    .residents.every(oltherThan => oltherThan.age >= age);
+
+  // base de modelagem
+  // const animalFilter = data.animals.find(typeName => typeName.name === animal);
+  // const ageAnimalFilter = animalFilter.residents.every(oltherThan => oltherThan.age >= age);
+  // return ageAnimalFilter;
 }
+
+console.table(animalsOlderThan('lions', 7));
 
 function employeeByName(employeeName) {
+  let findEmployeeName = {};
+  if (!employeeName){
+    return findEmployeeName;
+  }
+  findEmployeeName = data.employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
+  return findEmployeeName;
   // seu código aqui
 }
 
+console.log(employeeByName('Wilburn'));
+
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newObject = {};
+  Object.assign(newObject, personalInfo, associatedWith);
+  return newObject;
 }
 
 function isManager(id) {
+  // const idFilter = data.employees.filter( haveId => haveId.id === id);
+  // const managerFilter = idFilter.manager.find(haveManager => haveManager.managers === []);
+  // return managerFilter;
   // seu código aqui
 }
 
