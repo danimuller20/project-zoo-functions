2b20ea5eb97e6aee3e17a21351c7f0607d58815a
Rafael Pufal <rafael.pufal23@gmail.com>
2021-01-26 17:57:23 -0300

    refatorando schedule

--- src/zoo.js
+++ src/zoo.js
@@ -148,21 +148,12 @@ function animalMap(options) {
 // console.log(animalMap(options));
 
 function schedule(dayName) {
-  // const weeklyProgram = {
-  //   Tuesday: 'Open from 8am until 6pm',
-  //   Wednesday: 'Open from 8am until 6pm',
-  //   Thursday: 'Open from 10am until 8pm',
-  //   Friday: 'Open from 10am until 8pm',
-  //   Saturday: 'Open from 8am until 10pm',
-  //   Sunday: 'Open from 8am until 8pm',
-  //   Monday: 'CLOSED',
-  // };
   const weeklyProgram = {};
   Object.keys(hours).forEach((day) => {
     if (day !== 'Monday') {
-    weeklyProgram[day] = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`;
+      weeklyProgram[day] = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`;
     } else {
-    weeklyProgram[day] = 'CLOSED';
+      weeklyProgram[day] = 'CLOSED';
     }
   });
 
