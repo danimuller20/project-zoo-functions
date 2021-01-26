412b24b45c6a81aad093636b29060ece3f85f829
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-01-26 11:24:29 -0300

    Fix issues Code Climate

--- src/zoo.js
+++ src/zoo.js
@@ -37,10 +37,10 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
- return employees.some(funcionario => funcionario.managers.some(funcionarioId => funcionarioId === id));
+  return employees.some(funcionario => 
+    funcionario.managers.some(funcionarioId => funcionarioId === id));
+};
 
-}
-console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
