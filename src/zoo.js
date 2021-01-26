0ae4b50ee66626c57374600abd1ce857181be025
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-26 15:11:48 -0300

    Faz o exercício 6/code climate/2

--- src/zoo.js
+++ src/zoo.js
@@ -72,7 +72,7 @@ function isManager(id) {
 }
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
-  const newEmployee = {id, firstName, lastName, managers, responsibleFor};
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
   return data.employees.push(newEmployee);
 }
 
