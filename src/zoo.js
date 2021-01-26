3b0ed6e1bb1fa1e5f387f713883c00bc09630811
Bernardo Smith <bernardostech@gmail.com>
2021-01-25 21:29:33 -0500

    questao2

--- src/zoo.js
+++ src/zoo.js
@@ -33,7 +33,7 @@ return animals
 console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'))
 function animalsOlderThan(animal, age) {
   // seu código aqui
-return animals.every((animal, age) => animal.residents.age > age);
+return animals.every((animal, age) => animal.residents.age >= age);
 }
 console.log(animalsOlderThan('penguins', 10));
 
