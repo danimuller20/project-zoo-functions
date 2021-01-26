d4b233d093c4e60a076c9f3b13c3dc4a2da20505
Felipe Rocha <felipe.sr27@gmail.com>
2021-01-25 23:55:50 -0300

    Codeclimate: fixed issues in the js code

--- src/zoo.js
+++ src/zoo.js
@@ -72,38 +72,41 @@ function entryCalculator(entrants) {
 const getLocation = (acc, curr) => {
   acc[curr.location] = '';
   return acc;
-}
+};
 
 function animalMap(options) {
-  let zooMap = animals.reduce(getLocation, {});
-  Object.keys(zooMap).forEach(key =>
-    zooMap[key] = animals.filter(({ location }) => location === key).map(({ name }) => name));
+  const zooMap = animals.reduce(getLocation, {});
+  Object.keys(zooMap).forEach(key => {
+    zooMap[key] = animals.filter(({ location }) => location === key).map(({ name }) => name)
+  });
   if (options === undefined) {
     return zooMap;
   }
   if ((options.includeNames) && (options.sex === undefined)) {
-    Object.keys(zooMap).forEach(key =>
-      zooMap[key].forEach((animal, index) => 
-        zooMap[key][index] = { [animal]: animals.find(({ name }) => name === animal).residents.map(({ name }) => name) }));
+    Object.keys(zooMap).forEach(key => {
+      zooMap[key].forEach((animal, index) =>
+        zooMap[key][index] = { [animal]: animals.find(({ name }) => 
+          name === animal).residents.map(({ name }) => name) });
+    });
   }
   if ((options.includeNames) && (options.sex)) {
-    Object.keys(zooMap).forEach(key =>
-      zooMap[key].forEach((animal, index) => 
-        zooMap[key][index] = { [animal]: animals.find(({ name }) => name === animal).residents.filter(({ sex }) => sex === options.sex).map(({ name }) => name) }));
+    Object.keys(zooMap).forEach(key => {
+      zooMap[key].forEach((animal, index) =>
+        zooMap[key][index] = { [animal]: animals.find(({ name }) => 
+        name === animal).residents.filter(({ sex }) => 
+        sex === options.sex).map(({ name }) => name) })
+    });
   }
   if ((options.includeNames) && (options.sorted)) {
     Object.keys(zooMap).forEach(key =>
-      zooMap[key].forEach((animal, index) => {
-        Object.keys(animal).forEach(key => {
-          animal[key].sort();
+      zooMap[key].forEach((animal) => {
+        Object.keys(animal).forEach(key2 => {
+          animal[key2].sort();
         });
       }));
   }
   return zooMap;
 }
-
-const result = animalMap({ sex: 'female', sorted: true});
-console.log(result['NE']);
 
 function schedule(dayName) {
   // seu código aqui
