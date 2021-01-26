2f6639f9a592d9cd830f0b4bb6b32a717df2cdc8
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-01-26 07:26:05 -0300

    fabio

--- src/zoo.js
+++ src/zoo.js
@@ -87,20 +87,21 @@ function increasePrices(percentage) {
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  const employeeByNames = (employeeName) => {
+    const foundEmployee = employees.find(
+      employee => Object.values(employee).includes(employeeName));
+    return { ...foundEmployee };
+  }
   const getEmployeesInfo = (employeeInfo) => {
     let employeesQuery;
     if (employeeInfo) {
-      employeesQuery = [employeeByName(employeeInfo)];
+      employeesQuery = [employeeByNames(employeeInfo)];
     } else {
       employeesQuery = employees;
     }
     return employeesQuery;
   }
-  const employeeByName = (employeeName) => {
-    const foundEmployee = employees.find(employee => Object.values(employee).includes(employeeName));
-    return { ...foundEmployee };
-  }
-
+  const getAnimalsNamesByIds = animalsIds => animalsByIds(...animalsIds).map(animal => animal.name);
   const employeesQuery = getEmployeesInfo(idOrName);
   return employeesQuery.reduce((employeesObject, { firstName, lastName, responsibleFor }) => {
     employeesObject[`${firstName} ${lastName}`] = getAnimalsNamesByIds(responsibleFor);
