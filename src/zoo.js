712c2b7e0cc94f77909547b18b6ec08b28f8aba1
Felipe Rocha <felipe.sr27@gmail.com>
2021-01-26 09:18:34 -0300

    Codeclimate: fixed issues in the js code

--- src/zoo.js
+++ src/zoo.js
@@ -77,7 +77,7 @@ const getLocation = (acc, curr) => {
 function animalMap(options) {
   const zooMap = animals.reduce(getLocation, {});
   Object.keys(zooMap).forEach(key => {
-    zooMap[key] = animals.filter(({ location }) => location === key).map(({ name }) => name)
+    zooMap[key] = animals.filter(({ location }) => location === key).map(({ name }) => name);
   });
   if (options === undefined) {
     return zooMap;
@@ -85,28 +85,25 @@ function animalMap(options) {
   if ((options.includeNames) && (options.sex === undefined)) {
     Object.keys(zooMap).forEach(key => {
       zooMap[key].forEach((animal, index) =>
-      zooMap[key][index] = { [animal]: animals.find(({ name }) => 
-      name === animal).residents.map(({ name }) => name) });
+      zooMap[key][index] = { [animal]: animals.find(({ name }) => name === animal).residents.map(({ name }) => name)});
     });
   }
   if ((options.includeNames) && (options.sex)) {
     Object.keys(zooMap).forEach(key => {
       zooMap[key].forEach((animal, index) =>
-      zooMap[key][index] = { [animal]: animals.find(({ name }) => 
-      name === animal).residents.filter(({ sex }) => 
-      sex === options.sex).map(({ name }) => name) })
+      zooMap[key][index] = { [animal]: animals.find(({ name }) => name === animal).residents.filter(({ sex }) => 
+        sex === options.sex).map(({ name }) => name) })
     });
   }
   if ((options.includeNames) && (options.sorted)) {
     Object.keys(zooMap).forEach(key =>
-      zooMap[key].forEach((animal) => {
-      Object.keys(animal).forEach(key2 => {
-        animal[key2].sort();
-      });
+      zooMap[key].forEach((animal) => { Object.keys(animal).forEach(key2 => { animal[key2].sort();});
     }));
   }
   return zooMap;
 }
+
+console.log(animalMap());
 
 function schedule(dayName) {
   // seu código aqui
