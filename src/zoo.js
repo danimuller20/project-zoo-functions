eba815c6e718c04453eb5f6a5fa5499b00458d9d
Bernardo Smith <bernardostech@gmail.com>
2021-01-26 21:48:20 -0500

    questao2 ajustes

--- src/zoo.js
+++ src/zoo.js
@@ -12,36 +12,29 @@ eslint no-unused-vars: [
 const { animals, employees } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
-return animals
-.filter(animal => animal.id === ids )
-//.map(animal => animal)
-.map( animals => ({
- id : animals.id,
- name : animals.name,
- popularity: animals.popularity,
- location : animals.location,
- residents : ( {
-  name : animals.residents.name,
-  sex : animals.residents.sex,
-  age : animals.residents.age
- })
-}))
-
- }
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'))
+function animalsByIds(...ids) {
+  /*
+if (ids === undefined) {
+  return [];
+}
+const foundAnimals = ids.find(actualId => {
+  return actualId === animal.id).animals.find((animal) =>{
+})
+  return foundAnimals;
+})
+}
+*/
+}
 function animalsOlderThan(animal, age) {
   // seu código aqui
-return animals
-.find(animalName => animalName.name === animal )
-.every(ageGiven => animal.residents.age >= ageGiven);
+  return animals
+.find(animalName => animalName.name === animal).residents
+.every(ageGiven => ageGiven.age >= age);
 }
-console.log(animalsOlderThan('penguins', 10));
+console.log(animalsOlderThan('otters', 7));
 
 function employeeByName(employeeName) {
   // seu código aqui
-
 }
 
 function createEmployee(personalInfo, associatedWith) {
@@ -52,6 +45,7 @@ function isManager(id) {
   // seu código aqui
   return employees.some(number => number.managers === id);
 }
+
 console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
