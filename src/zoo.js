0945fb3b29c8e57843e069296f6bc043d0118398
Victor Ricco <victor.ricco001@gmail.com>
2021-01-27 00:34:50 -0300

    entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -52,19 +52,46 @@ function isManager(id) {
   }
   return true;
 }
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(newEmployee);
+  return newEmployee;
 }
-
-function animalCount(species) {
+//console.log(addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+//[
+//  '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+//  'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+//],
+//[
+//  'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+//  '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+//]));
+function animalCount(species = undefined) {
   // seu código aqui
+  if (species === undefined) {
+    data.animals.forEach(currentValue => currentValue.name + currentValue.residents.length);
+  }
+  return data.animals.find(currentValue => currentValue.name === species).residents.length;
 }
-
-function entryCalculator(entrants) {
+//console.log(animalCount('lions'));
+function entryCalculator(entrants = undefined) {
   // seu código aqui
+  let fullPrice = 0
+  if (entrants === undefined) {
+    return 0;
+  }
+  Object.keys(entrants).forEach(key => fullPrice += data.prices[key] * entrants[key]);
+  return fullPrice;
 }
-
+console.log(entryCalculator({}));
 function animalMap(options) {
   // seu código aqui
 }
