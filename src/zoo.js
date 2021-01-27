eca3f31b829fdd731b81d58edf5d4d8e0e3afcf9
Bernardo Smith <bernardostech@gmail.com>
2021-01-26 22:44:23 -0500

    questao1 ajustes

--- src/zoo.js
+++ src/zoo.js
@@ -13,18 +13,9 @@ const { animals, employees } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  /*
-if (ids === undefined) {
-  return [];
+  return animals.filter((animal, index) => animal.id === ids[index]);
 }
-const foundAnimals = ids.find(actualId => {
-  return actualId === animal.id).animals.find((animal) =>{
-})
-  return foundAnimals;
-})
-}
-*/
-}
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 function animalsOlderThan(animal, age) {
   // seu código aqui
   return animals
