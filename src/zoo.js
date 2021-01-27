790f7fba3fdfbfd726745724ee6dd6f8dab3db70
Victor Ricco <victor.ricco001@gmail.com>
2021-01-26 22:43:47 -0300

    Code Climate

--- src/zoo.js
+++ src/zoo.js
@@ -45,12 +45,12 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  const manager = data.employees.find(employeeId => 
-    employeeId.managers.find(currentValue => currentValue === id)=== id)
+  const manager = data.employees.find(employeeId =>
+    employeeId.managers.find(currentValue => currentValue === id) === id);
   if (manager === undefined) {
-    return false
+    return false;
   }
-  return true
+  return true;
 }
 console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
