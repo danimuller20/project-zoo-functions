09a6f50bbef04c422881fcaafcaf52c7719ba6d0
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-01-26 23:18:35 -0300

    Lesson: codeClimate

--- src/zoo.js
+++ src/zoo.js
@@ -36,9 +36,9 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return employees
-  .filter(associatedWith => associatedWith.managers && associatedWith.responsibleFor).employees.find(personalInfor => personalInfo.id && personalInfo.firstName && personalInfo.lastName);
-};
+  // return employees
+  // .filter(associatedWith => associatedWith.managers && associatedWith.responsibleFor).employees.find(personalInfor => personalInfo.id && personalInfo.firstName && personalInfo.lastName);
+}
 
 function isManager(id) {
   // seu código aqui
@@ -71,18 +71,18 @@ function oldestFromFirstSpecies(id) {
 function increasePrices(percentage) {
   // seu código aqui
 }
-//Questões resolvidas no plantão com Murilo e Bernardo
+// Questões resolvidas no plantão com Murilo e Bernardo
 function getAnimalListFromEmplyee(employee) {
   return employee.responsibleFor
-    .map((animalId) => animals.find((animal) => animalId === animal.id).name);
-    //['id1', 'id2'] => ['lion', 'tinger']
+    .map(animalId => animals.find((animal) => animalId === animal.id).name);
+    // ['id1', 'id2'] => ['lion', 'tinger']
 }
 function getEmployeeFullName(employee) {
   return `${employee.firstName} ${employee.lastName}`;
 }
 
 function getAllEmployeesAndAnimals() {
-  return employees.reduce((accumulator, employee) => {
+  return employees.reduce(accumulator, employee => {
     const animalList = getAnimalListFromEmplyee(employee);
     accumulator[getEmployeeFullName(employee)] = animalList;
     return accumulator;
@@ -90,22 +90,21 @@ function getAllEmployeesAndAnimals() {
 }
 function getEmployeedByNameOrId(idOrName) {
   return employees
-  .find((employee) => employee.id === idOrName
+  .find(employee => employee.id === idOrName
     || employee.firstName === idOrName || employee.lastName === idOrName);
 }
 
 function employeeCoverage(idOrName) {
-  //Sem parâmetros, retorna uma lista de funcionários e os animais pelos quais são responsáveis
-  if(!idOrName) {
-    //firstName, lastName de employees, responsibleFor
+  // Sem parâmetros, retorna uma lista de funcionários e os animais pelos quais são responsáveis
+  if (!idOrName) {
+  // firstName, lastName de employees, responsibleFor
     return getAllEmployeesAndAnimals();
   }
-    const targetEmployee = getEmployeedByNameOrId(idOrName);
+  const targetEmployee = getEmployeedByNameOrId(idOrName);
     const animalList = getAnimalListFromEmplyee(targetEmployee);
-    const key = getEmployeeFullName(targetEmployee);
-    return { [key]: animalList };
-
-  }
+  const key = getEmployeeFullName(targetEmployee);
+  return { [key]: animalList };
+}
 
 
 module.exports = {
