d476256ba89ca95475dfc6c83af563fd8ec4dff3
jiarguello <jhonatan.arguello@gmail.com>
2021-01-27 00:02:21 -0300

    Iniciando o exercicio 2. Rapaz, ta dificil.

--- src/zoo.js
+++ src/zoo.js
@@ -16,9 +16,21 @@ function animalsByIds(...ids) {
   return animals.filter((animal, index) => ids[index] === animal.id);
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
+function animalsOlderThan(animal, age) { 
+  //Verificar se é 'true' o retorno de um filtro realizado por espécie e idade mínima
+  //1. Buscar o objeto contendo os animais; [CHECK]
+  //2. Buscar nesse objeto a espécie indicada no parâmetro da função;
+  //3. Acessar a idade de cada um dos animais da espécie
+  //4. Verificar se essa idade é superior ao parâmetro passado
+  //5. Retornar true se todos os animais atenderem aos critérios, senão retorna false
+
+  const allAnimals = data.animals
+
+  return allAnimals.find((animal) => animal === 'lions');
+ 
 }
+
+animalsOlderThan('lions', 4)
 
 function employeeByName(employeeName) {
   // seu código aqui
