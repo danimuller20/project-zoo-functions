2365681c879221659ba700d30ac657e0a5ae1724
Bernardo Smith <bernardostech@gmail.com>
2021-01-27 11:24:29 -0500

    questao6  ajuste

--- src/zoo.js
+++ src/zoo.js
@@ -57,7 +57,7 @@ function isManager(id) {
 }
 console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   const employeeAdded = {};
   employeeAdded.id = id;
   employeeAdded.firstName = firstName;
