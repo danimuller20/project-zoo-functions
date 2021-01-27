b50756860365c15d64b5dc7cc02bf25b31f489db
Victor Ricco <victor.ricco001@gmail.com>
2021-01-26 22:06:26 -0300

    CreateEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -39,6 +39,8 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const newEmployee = {...personalInfo, ...associatedWith};
+  return newEmployee;
 }
 
 function isManager(id) {
