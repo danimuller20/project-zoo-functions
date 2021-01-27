e3f728ff0832b1d7834c261b58e7c5349a98ed1b
Bernardo Smith <bernardostech@gmail.com>
2021-01-27 00:09:31 -0500

    questao7

--- src/zoo.js
+++ src/zoo.js
@@ -35,17 +35,34 @@ function employeeByName(employeeName) {
 console.log(employeeByName('Wishart'));
 
 function createEmployee(personalInfo, associatedWith) {
-  const newEmployee = [...personalInfo, ...associatedWith];
-  return newEmployee;
+  const newEmployeeInfo = { ...personalInfo, ...associatedWith };
+  data.employees.push(newEmployeeInfo);
+  return employees.find(employee => employee[employee.length-1]);
 }
-
+console.log(createEmployee({
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+}, {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'],
+}));
 function isManager(id) {
   return employees.some(employee => employee.managers.includes(id));
 }
 console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  data.employees.id.push(id);
+  data.employees.firstName.push(firstName);
+  data.employees.lastName(lastName);
+  data.employees.managers(managers);
+  data.employees.responsibleFor(responsibleFor);
 }
 
 function animalCount(species) {
