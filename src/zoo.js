602015cd55aa261747c1e337bd99fbc0a6eb1bbd
jlfagundes <jlfagundes@gmail.com>
2021-01-27 12:16:35 -0300

    Function animalMap feita

--- src/zoo.js
+++ src/zoo.js
@@ -73,7 +73,81 @@ function animalMap(options) {
   // a função retorna um objeto
   // a função recebe um objeto
   // esse objeto possui a seguinte entrada chave=localização e valor=array
-}
+  function returnLocations() {
+    const locations = [];
+    animals.forEach((element) => {
+      if (!locations.includes(element.location)) {
+        locations.push(element.location);
+      }
+    });
+    return locations;
+  }
+  
+  function searchAnimalsByLocation(location) {
+    const arrayOfAnimalsByLocation = animals
+    .filter(animal => animal.location === location)
+    .map(element => element.name);
+    return arrayOfAnimalsByLocation;
+  }
+  
+  function searchResidentsNamesByEspecie(especieName) {
+    return animals
+    .reduce((residents, animal) => {
+      if (animal.name === especieName) {
+        residents[especieName] = animal.residents.map(resident => resident.name);
+      }
+      return residents;
+    }, {});
+  }
+  
+  function sortNames(object) {
+    Object.keys(object).forEach((location) => {
+      object[location].forEach((especie) => {
+        especie[Object.keys(especie)[0]].sort();
+      });
+    });
+    return object;
+  }
+  function separateAnimalsBySex(animalName, sex) {
+    const arrayAnimalBySex = animals.reduce((arrayAnimalBySexConstruction, element) => {
+      if (element.name === animalName) {
+        arrayAnimalBySexConstruction.push(element.residents
+          .filter(resident => resident.sex === sex));
+      }
+      return arrayAnimalBySexConstruction;
+    }, []);
+    return arrayAnimalBySex[0].map(value => value.name);
+  }
+  const arrayOfLocations = returnLocations();
+  function createDefaultObject() {
+    return arrayOfLocations.reduce((defaultObject, location) => {
+      defaultObject[location] = searchAnimalsByLocation(location);
+      return defaultObject;
+    }, {});
+  }
+  function createObjectWithNames(optionSex = false) {
+    const objectWithNames = arrayOfLocations.reduce((objectWithNamesByEspecie, location) => {
+      const arrayWithNames = searchAnimalsByLocation(location).map(searchResidentsNamesByEspecie);
+      objectWithNamesByEspecie[location] = arrayWithNames;
+      return objectWithNamesByEspecie;
+    }, {});
+    if (optionSex) {
+      Object.keys(objectWithNames).forEach((location) => {
+        objectWithNames[location].forEach((animal) => {
+          animal[Object.keys(animal)[0]] = separateAnimalsBySex(Object.keys(animal)[0], optionSex);
+        });
+      });
+      return objectWithNames;
+    }
+    return objectWithNames;
+  }
+  function animalMap(options) {
+    if (!options || !options.includeNames) {
+      return createDefaultObject();
+    }
+    return (options.sorted ? sortNames(createObjectWithNames(options.sex))
+    : createObjectWithNames(options.sex));
+  }
 
 function schedule(dayName) {
   // seu código aqui
