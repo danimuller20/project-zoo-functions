ae21208cd439565d11aa8609e20256f65b74c4cb
jiarguello <jhonatan.arguello@gmail.com>
2021-01-27 21:31:03 -0300

    Resoluçao de issues, incluindo a refatoraçao do requisito 3.

--- src/zoo.js
+++ src/zoo.js
@@ -9,28 +9,48 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
+const { animals, employees } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
   return animals.filter((animal, index) => ids[index] === animal.id);
 }
 
-function animalsOlderThan(animal, age) { 
-  //Verificar se é 'true' o retorno de um filtro realizado por espécie e idade mínima
-  //1. Buscar o objeto contendo os animais; [CHECK]
-  //2. Buscar nesse objeto a espécie indicada no parâmetro da função;
-  //3. Acessar a idade de cada um dos animais da espécie
-  //4. Verificar se essa idade é superior ao parâmetro passado
-  //5. Retornar true se todos os animais atenderem aos critérios, senão retorna false
+function animalsOlderThan(animal, age) {
+  /*
+  Verificar se é 'true' o retorno de um filtro realizado por espécie e idade mínima
+  1. Buscar o objeto contendo os animais;
+  2. Buscar nesse objeto a espécie indicada no parâmetro da função;
+  3. Acessar a idade de cada um dos animais da espécie
+  4. Verificar se essa idade é superior ao parâmetro passado
+  5. Retornar true se todos os animais atenderem aos critérios, senão retorna false
+  */
 
-  const findSpecies = animals.find(item => item.name === animal).residents
+  const findSpecies = animals.find(element => element.name === animal).residents;
 
-  return findSpecies.every((element) => element.age > age)
+  return findSpecies.every(element => element.age > age);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  /*
+  Objetivo: Retornar o objeto do funcionário.
+  Deverá ser possível pesquisar por nome ou sobrenome.
+  Caso não seja indicado um nome ou sobrenome retornar um objeto vazio.
+
+  1 - Acessar o objeto employee
+  2 - Buscar pelo nome ou sobre nome
+  3 - Retornar o objeto
+  */
+
+  const employeeReturn = employees.find(
+    element => element.firstName === employeeName || element.lastName === employeeName);
+
+  const emptObject = {};
+
+  if (employeeName === undefined) {
+    return emptObject;
+  }
+  return employeeReturn;
 }
 
 function createEmployee(personalInfo, associatedWith) {
