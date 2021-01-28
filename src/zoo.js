8cb84ed2fb14db8c7135aa3424ef7084fb58d110
MZTBTG <mztbtg@hotmail.com>
2021-01-28 14:47:57 -0300

    Correção do exercício employeeByName - codeClimate 01

--- src/zoo.js
+++ src/zoo.js
@@ -17,13 +17,13 @@ function animalsByIds(ids) {
 
 function animalsOlderThan(animal, age) {
   const result = data.animals.find(especie => especie.name === animal);
-  return result.residents.every( residente => residente.age > age);
-  
+  return result.residents.every(residente => residente.age > age);
 }
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};
-  const result = data.employees.find(element => element.firstName === employeeName || element.lastName === employeeName);
+  const result = data.employees.find(element => {
+    return element.firstName === employeeName || element.lastName === employeeName});
   return result;
 }
 employeeByName('Nigel');
