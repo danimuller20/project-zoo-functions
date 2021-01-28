c543a70dcb7c0f8803aac2dad307330d6417d110
Enio Nicael <enionicael@gmail.com>
2021-01-28 11:54:27 -0300

    Requisito 4 implementa a funcao createEmployee.

--- src/zoo.js
+++ src/zoo.js
@@ -33,7 +33,7 @@ function employeeByName(employeeName) {
 } // concluido
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
