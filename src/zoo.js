30704390a9352796c702ee9d510257496ebfb9d7
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2021-01-28 18:25:16 -0300

    ex2

--- src/zoo.js
+++ src/zoo.js
@@ -13,12 +13,27 @@ const data = require('./data');
 
 //Iniciando:
 
+//1 IMPLEMENTE A FUNÇÃO animalsByIds
+//Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+
 function animalsByIds(ids) {
   // seu código aqui
 }
 
+//2 IMPLEMENTE A FUNÇÃO animalsOlderThan
+//Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada
+
+//
+
+
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalBySpecie = animals.find(({ name }) => {
+    return name === animal;
+ })
+
+ return animalBySpecie.residents.every((preciousValue) => {
+   preciousValue.age <= age;
+ })
 }
 
 function employeeByName(employeeName) {
