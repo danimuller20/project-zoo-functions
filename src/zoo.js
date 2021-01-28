7a163c436c2defe3ef6f6adb560bfbe62773a075
MZTBTG <mztbtg@hotmail.com>
2021-01-28 15:34:45 -0300

    Correção do exercício employeeByName - codeClimate 07

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   return data.animals.filter(animal => ids.some(id => animal.id === id));
 }
 
