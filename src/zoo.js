72a40a8dc9f4a2366ab191351c1b858c64e57a75
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-01-27 22:13:32 -0300

    IMPLEMENTE A FUNÇÃO entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -11,12 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees } = data;
+const { animals, employees, hours, prices } = data;
 
 function animalsByIds(...ids) {
   return animals.filter((value, index) => value.id === ids[index]);
 }
-
 function animalsOlderThan(animal, age) {
   return animals.find(value => value.name === animal).residents.every(idade => idade.age > age);
 }
@@ -50,17 +49,38 @@ function animalCount(species) {
   return species ? obj[species] : obj;
 }
 
+// A partir da quantidade de visitantes e a faixa etária de cada um, esta função é responsável por retornar o preço total a ser cobrado
+// 1. Buscar a quantidade a ser cobrada no ingresso por cada faixa etária
+// 2. Conferir a quantidade de pessoas e somar os valores referente ao preço dos ingressos.
+// 3. Parâmetro deve receber um objeto
+// {
+//  Adult: quantidade de pessoas
+//  Child: quantidade de pessoas
+//  Senior: quantidade de pessoas
+// }
+// 4. Retorna 0 se nenhum argumento for passado
+// 5. Retorna 0 se um objeto vazio for passado
+// 6. Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0
+  }
+  let result = 0;
+  const value = Object.values(entrants);
+  const persons = Object.keys(entrants);
+  persons.forEach((person, index) => {
+   result += prices[person] * value[index]
+  })
+  return result
 }
-
+console.log(entryCalculator());
 function animalMap(options) {
   // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
+
 }
 
 function oldestFromFirstSpecies(id) {
