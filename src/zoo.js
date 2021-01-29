f87c49939c8e8ac2c7475518953c7c1fde9e637e
MZTBTG <mztbtg@hotmail.com>
2021-01-29 12:45:42 -0300

    Adicionando diversas funções

--- src/zoo.js
+++ src/zoo.js
@@ -27,25 +27,46 @@ function employeeByName(employeeName) {
   return rst;
 }
 
-employeeByName('Nigel');
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployee = { ...personalInfo, ...associatedWith };
+  return newEmployee;
 }
 
 function isManager(id) {
-  // seu código aqui
+  function isManegerHere(employe, compareID) {
+    return employe.managers.some(manegerId => manegerId === compareID);
+  }
+  return employees.some(employe => isManegerHere(employe, id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor };
+  data.employees.push(newEmployee);
+  console.log(newEmployee);
 }
 
 function animalCount(species) {
-  // seu código aqui
+  return species ? getResidentNumber(species) : getNumbersOfEachAnimal();
 }
 
-function entryCalculator(entrants) {
-  // seu código aqui
+function entryCalculator(entrants = 0) {
+  const pricesData = data.prices;
+  const pricesCategories = Object.keys(pricesData);
+  const totalPrice = pricesCategories.reduce((accumulator, priceCategory) => {
+    const priceToAdd =
+    entrants[priceCategory] ? entrants[priceCategory] * pricesData[priceCategory] : 0;
+
+    const priceToPay = accumulator + priceToAdd;
+    return priceToPay;
+  }, 0);
+  console.log(...pricesCategories);
+  console.log(totalPrice);
+  return totalPrice;
 }
 
 function animalMap(options) {
@@ -57,11 +78,24 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeData = data.employees.find(employee => employee.id === id);
+  const firstAnimalID = employeeData.responsibleFor[0];
+  const animalResidents = data.animals.find(animal => animal.id === firstAnimalID).residents;
+  const oldestAnimalAge = animalResidents.reduce((accumulator, resident) =>
+    ((resident.age > accumulator) ? resident.age : accumulator), 0);
+  const oldestAnimalResident = animalResidents.find(animal => animal.age === oldestAnimalAge);
+  const { name, sex, age } = oldestAnimalResident;
+  const returnData = [name, sex, age];
+  return returnData;
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const pricesOptions = Object.keys(data.prices);
+  const priceRateToChange = 1 + (percentage / 100);
+  pricesOptions.forEach(priceOption =>
+  (data.prices[priceOption] =
+    (Math.round((data.prices[priceOption] * priceRateToChange) * 100)) / 100));
+  console.log(data.prices);
 }
 
 function employeeCoverage(idOrName) {
