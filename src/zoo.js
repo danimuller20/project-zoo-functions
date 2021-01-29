5c51b14b8398d294b396ab61e5fc5b5ea74b3a47
Pedro Toselli <pedrotrybe@gmail.com>
2021-01-29 12:47:33 -0300

    Requisito 1 - Code climet

--- src/zoo.js
+++ src/zoo.js
@@ -13,9 +13,25 @@ const data = require('./data');
 
 const { animals } = data;
 
+// IMPLEMENTE A FUNÇÃO animalsByIds
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+// Observações técnicas
+
+// O parâmetro desta função pode ser alterado para atender ao requisito proposto
+// O que será avaliado
+
+// Caso receba nenhum parâmetro, necessário retornar um array vazio
+// Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id
+// animal.ids  retorna -> [especie] 
+// Ao receber mais de um id, retorna um array com as espécies referentes aos ids
+// animal.ids  retorna -> [especies] 
+
+
 function animalsByIds(...ids) {
-  return animals.filter(animal=>ids.some(id=>(id === animal.id)));
+  return animals.filter(animal => ids.some(id => id === animal.id));
 }
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
