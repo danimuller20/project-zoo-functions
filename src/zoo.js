f64b97ed2433a51fb1d307aca0ba06e78f1bd510
Matheus Monteiro <monteiromsh@gmail.com>
2021-01-30 02:33:46 -0300

    primeiro requisito

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,35 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  // 1- Caso receba nenhum parâmetro, necessário retornar um array vazio:
+  const idsListMap = data.animals.map((element) => element.id);
+
+  if (ids.length === 0) {
+    return [];
+  }
+
+  // 2- Se o parâmetro é um único id, retorna um array com a espécie referente à esse id:
+  else if (ids.length === 1) {
+    const findIdOnMap = idsListMap.find((element) => element === ids[0]);
+    const idsOnObj = data.animals.find((element) => element.id === findIdOnMap);
+    return [idsOnObj];
+  }
+
+  // 3- Ao receber mais de um id, retorna um array com as espécies referentes aos ids:
+  else {
+    const arrayAux = [];
+    for (let index = 0; index < ids.length; index += 1) {
+      const teste = data.animals.find((element) => element.id === ids[index]);
+      arrayAux.push(teste);
+    }
+    return arrayAux;
+  }
 }
+animalsByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+  'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'
+);
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
