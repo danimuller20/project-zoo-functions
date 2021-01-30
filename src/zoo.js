5acc2ec7ae024531512a0a9d0ced48da3801d18f
Matheus Monteiro <monteiromsh@gmail.com>
2021-01-30 02:46:04 -0300

    primeiro requisito - corrigido #2

--- src/zoo.js
+++ src/zoo.js
@@ -15,27 +15,25 @@ function animalsByIds(...ids) {
   // 1- Caso receba nenhum parâmetro, necessário retornar um array vazio:
   const idsListMap = data.animals.map(element => element.id);
 
-  if (ids.length === 0) 
-  { 
-    return []; 
+  if (ids.length === 0) {
+    return [];
   }
 
   // 2- Se o parâmetro é um único id, retorna um array com a espécie referente à esse id:
-  else if (ids.length === 1) 
-  {
+  else if (ids.length === 1) {
     const findIdOnMap = idsListMap.find(element => element === ids[0]);
     const idsOnObj = data.animals.find(element => element.id === findIdOnMap);
     return [idsOnObj];
   }
 
   // 3- Ao receber mais de um id, retorna um array com as espécies referentes aos ids:
-    const arrayAux = [];
-    for (let index = 0; index < ids.length; index += 1) {
-      const teste = data.animals.find((element) => element.id === ids[index]);
-      arrayAux.push(teste);
-    }
-    return arrayAux;
+  const arrayAux = [];
+  for (let index = 0; index < ids.length; index += 1) {
+    const teste = data.animals.find(element => element.id === ids[index]);
+    arrayAux.push(teste);
   }
+  return arrayAux;
+}
 animalsByIds(
   '0938aa23-f153-4937-9f88-4858b24d6bce',
   'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
