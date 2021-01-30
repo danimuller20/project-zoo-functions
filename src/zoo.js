01d05087ef0c369bbabb6f1a66736cc7993f9a8e
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-01-30 12:15:16 -0300

    animalsOlderThan - correçao codeClimate, comentado animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -12,33 +12,25 @@ eslint no-unused-vars: [
 const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(...args) {
-  if (args.length === 0) return [];
+function animalsByIds(args) {
+  // if (args.length === 0) return [];
 
-  const array = [];
+  // const array = [];
 
-  args.forEach((cadaElemento) => {
-    return animals.map((item) => {
-      if (item.id === cadaElemento) {
-        array.push(item);
-      }
-    })});
+  // args.forEach((cadaElemento) => {
+  //   return animals.map((item) => {
+  //     if (item.id === cadaElemento) {
+  //       array.push(item);
+  //     }
+  //   })});
 
-  return array;
+  // return array;
 }
 
 function animalsOlderThan(especie, age) {
-  let bixo = data.animals.find((animal) => {
-    return animal.name === especie;
-  });
-
-  let idade = bixo.residents.map((idade) => {
-    return idade.age >= age;
-  });
-
-  let resultado = idade.find((item) => {
-    return item === false;
-  });
+  const bixo = data.animals.find(animal => animal.name === especie);
+  const idade = bixo.residents.map(idade => idade.age >= age);
+  const resultado = idade.find(item => item === false);
 
   if (resultado === undefined) {
     return true;
