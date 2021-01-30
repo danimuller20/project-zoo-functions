b432dc1c234939d14d4f4e14de909ff013abb6ee
Marina Alane <marina.alane@outlook.com>
2021-01-30 14:33:27 -0300

    ajusta cc

--- src/zoo.js
+++ src/zoo.js
@@ -47,7 +47,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function animalCount(species) {
   const animalsObj = {};
-  animals.forEach((bicho) => { animalsObj[bicho.name] = bicho.residents.length ;});
+  animals.forEach((bicho) => { animalsObj[bicho.name] = bicho.residents.length; });
   if (species) {
     return animalsObj[species];
   }
