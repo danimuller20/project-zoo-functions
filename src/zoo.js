1c1158a9d028b68b1e0f6bffdbe1fc87e99b78dd
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-31 10:28:48 -0300

    Décimo Requisito, com correções do Codeclimate 3

--- src/zoo.js
+++ src/zoo.js
@@ -80,12 +80,12 @@ function schedule(dayName) {
     const { open, close } = val;
     acumulador[key] = close - open > 0 ? `Open from ${open}am until ${close % 12}pm` : 'CLOSED';
     return acumulador;
-    }, {});
+  }, {});
   if (typeof dayName === 'string' && dayName.length !== 0) {
     return {
       [dayName]: result[dayName],
-  };
-}
+    };
+  }
   return result;
 }
 
