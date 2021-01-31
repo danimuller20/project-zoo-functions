e2424f087313474ffb6d4281b4bda1ab8bdcb280
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-31 10:15:11 -0300

    Décimo Requisito, com correções do NPM Test

--- src/zoo.js
+++ src/zoo.js
@@ -76,7 +76,17 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const result = Object.entries(data.hours).reduce((acumulador, [key, val]) => {
+    const { open, close } = val;
+    acumulador[key] = close - open > 0 ? `Open from ${open}am until ${close % 12}pm` : 'CLOSED';
+    return acumulador;
+    }, {});
+    if (typeof dayName === 'string' && dayName.length !== 0) {
+    return {
+    [dayName]: result[dayName],
+    };
+    }
+    return result;  
 }
 
 function oldestFromFirstSpecies(id) {
