3b49a21e861ed37b6bdb81e57a4d9e8e9ab52f64
Renato Benage <renatobenage@gmail.com>
2021-02-01 13:46:10 -0300

    tentando resolver code

--- src/zoo.js
+++ src/zoo.js
@@ -98,29 +98,29 @@ function increasePrices(percentage) {
 
 }
 // Plantão com Murilo e Bernardo
-function getAnimalListFromEmplyee(employee) {
-  return employee.responsibleFor.map(
-    animalId => animals.find(animal => animalId === animal.id).name);
-}
+// function getAnimalListFromEmplyee(employee) {
+//   return employee.responsibleFor.map(
+//     animalId => animals.find(animal => animalId === animal.id).name);
+// }
 
-function getEmployeeFullName(employee) {
-  return `${employee.firstName} ${employee.lastName}`;
-}
+// function getEmployeeFullName(employee) {
+//   return `${employee.firstName} ${employee.lastName}`;
+// }
 
-function getAllEmployeesAndAnimals() {
-  return employees.reduce((accumulator, employee) => {
-    const animalList = getAnimalListFromEmplyee(employee);
-    accumulator[getEmployeeFullName(employee)] = animalList;
-    return accumulator;
-  }, {});
+// function getAllEmployeesAndAnimals() {
+//   return employees.reduce((accumulator, employee) => {
+//     const animalList = getAnimalListFromEmplyee(employee);
+//     accumulator[getEmployeeFullName(employee)] = animalList;
+//     return accumulator;
+//   }, {});
 
-  function getEmployeedByNameOrId(idOrName) {
-    return employees.find(
-      employee =>
-        employee.id === idOrName ||
-        employee.firstName === idOrName ||
-        employee.lastName === idOrName);
-  }
+//   function getEmployeedByNameOrId(idOrName) {
+//     return employees.find(
+//       employee =>
+//         employee.id === idOrName ||
+//         employee.firstName === idOrName ||
+//         employee.lastName === idOrName);
+//   }
 
 function employeeCoverage(idOrName) {
   // seu código aqui
