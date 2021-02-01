990ab76f28846e180781be1995eb497d5156cd9d
MarceloPelegrino <pelegrinocorrea@gmail>
2021-02-01 16:37:22 -0300

    Requisito animalsOlderThan Dia 01-02-2021 16:37

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 // Alteração para primeiro commit
 
-const { employees, animals } = require('./data');
+const { animals, employees } = require('./data');
 const data = require('./data');
 // const { employees, animals } = data;
 
@@ -20,18 +20,23 @@ function animalsByIds(...ids) {
     return [];
   }
   const foundAnimals = ids.map((id) => {
-    const foundAnimal = data.animals.find((animal) => {
-      return id === animal.id;
-    });
+    const foundAnimal = data.animals.find((animal) => id === animal.id);
     return foundAnimal;
   });
   return foundAnimals;
 }
-// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  // Recebe nome de uma espécie em string e uma idade em number
+  // Testa se todos animais desta espécie possuem a idade mínima especificada
+  // Retorna um valor booleano
+
+  const species = data.animals.find((species) => species.name === animal)
+  return species.residents.every((animalAge) => animalAge.age > age);
+  
 }
+// console.log(animalsOlderThan('otters', 7));
 
 function employeeByName(employeeName) {
   // seu código aqui
@@ -69,10 +74,10 @@ function retrieveAnimalsPerLocation(locations) {
   const animalsPerLocation = {};
   locations.forEach((location) => {
     const filteredAnimals = data.animals
-      .filter((animal) => animal.location === location )
-      .map((animal) => animal.name );
+      .filter(animal => animal.location === location)
+      .map(animal => animal.name);
 
-      animalsPerLocation[location] = filteredAnimals;
+    animalsPerLocation[location] = filteredAnimals;
   });
   return animalsPerLocation;
 }
@@ -81,11 +86,11 @@ function retrieveAnimalsPerLocationWithName(locations, sorted, sex) {
   const animalsPerLocation = {};
   locations.forEach((location) => {
     const filteredAnimals = data.animals
-      .filter((animal) => animal.location === location )
+      .filter(animal => animal.location === location)
       .map((animal) => {
         const nameKey = animal.name;
         const nameValue = animal.residents
-          .map((resident) => resident.name)
+          .map(resident => resident.name);
           // .filter((resident) => { resident.sex });
 
         // if (sorted) {
@@ -95,7 +100,7 @@ function retrieveAnimalsPerLocationWithName(locations, sorted, sex) {
         return { [nameKey]: nameValue };
       });
 
-      animalsPerLocation[location] = filteredAnimals;
+    animalsPerLocation[location] = filteredAnimals;
   });
   return animalsPerLocation;
 }
@@ -111,16 +116,20 @@ function animalMap(options) {
   // Valor com o parametro includeNames: um array de objetos com as especies e o nome delas
   // Valor com o parametro sorted: um array de objetos com as especies e o nome delas ORDENADA
   // Valor com a parametro sex: um array de objetos com as especies e nome delas FILTRADA POR SEXO
-  // Valor com o parametro sorted e sex: um array de objetos com as especies e o nome delas ORDENADA e FILTRADA POR SEXO
+  // Valor com o parametro sorted e sex: um array de objetos com as especies e o nome delas
+  // ORDENADA e FILTRADA POR SEXO
   // Valor sem includeNames: array de string com as especies
 
   // PSEUDO CODIGO
   // 1. Recuperar as regiões que eu quero categorizar CHECK
   // 2. Uma vez que eu tenho as regiões, eu filtro os animais dessa região CHECK
   // 3. Se não tiver parametros de opções, retorna um array com as especies CHECK
-  // 4. Se a opção includeNames estiver habilitada, retorna um array de objetos com as especies e o nome delas
-  // 5. Se a opção sorted estiver habilitada, retorna um array de objetos com as especies e o nome delas ORDENADA
-  // 6. Se a opção sex estiver habilitada, retorna um array de objetos com as especies e nome delas FILTRADA POR SEXO
+  // 4. Se a opção includeNames estiver habilitada, retorna um array de
+  // objetos com as especies e o nome delas
+  // 5. Se a opção sorted estiver habilitada, retorna um array de
+  // objetos com as especies e o nome delas ORDENADA
+  // 6. Se a opção sex estiver habilitada, retorna um array de
+  // objetos com as especies e nome delas FILTRADA POR SEXO
   // 7. Se a opção sex e sorted estiver habilitada retorna um array de objetos com as especies e o nome delas ORDENADA e FILTRADA POR SEXO
   // 8. Se não tiver a opção includeNames: retorna array de string com as especies
 
@@ -157,8 +166,10 @@ function change24HourFormatTo12Format(hour) {
 // **************************************
 function schedule(dayName) {
   // seu código aqui
-  // Ao chamar a função sem parametros -> retorna um objeto com o cronograma inteiro
-  // Ao chamar a função com 1 parametros (o dia da semana) -> retorna um objeto com o cronograma apenas do dia
+  // Ao chamar a função sem parametros -> retorna um
+  // objeto com o cronograma inteiro
+  // Ao chamar a função com 1 parametros (o dia da semana) -> retorna um
+  // objeto com o cronograma apenas do dia
   // A função schedule retorna um objeto
   // Formato do objeto de retorno
   // chave === dia da semana
@@ -186,7 +197,7 @@ function schedule(dayName) {
     if (day === 'Monday') {
       schedule[day] = 'CLOSED';
     } else {
-      schedule[day] = `Open from ${open}am until ${change24HourFormatTo12Format(close)}pm`;      
+      schedule[day] = `Open from ${open}am until ${change24HourFormatTo12Format(close)}pm`;
     }
   });
 
@@ -194,7 +205,7 @@ function schedule(dayName) {
     return schedule;
   }
 
-  return { [dayName]: schedule[dayName] } ;
+  return { [dayName]: schedule[dayName] };
 }
 
 function oldestFromFirstSpecies(id) {
@@ -209,8 +220,8 @@ function increasePrices(percentage) {
 // **************************************
 // **************************************
 function getAnimalListFromEmployee(employee) {
-  return employee.responsibleFor.map((animalId) => animals.find((animal) => animalId === animal.Id).name);
-//   // ['id1', 'id2'] => ['lion', 'tiger']
+  return employee.responsibleFor.map((animalId) => animals.find((animal) => animalId === animal.id).name);
+// //   // ['id1', 'id2'] => ['lion', 'tiger']
 }
 
 function getEmployeeFullName(employee) {
@@ -221,6 +232,7 @@ function getAllEmployeesAndAnimals() {
   return employees.reduce((accumulator, employee) => {
     const animalList = getAnimalListFromEmployee(employee);
     accumulator[getEmployeeFullName(employee)] = animalList;
+    return accumulator;
   }, {});
 }
 
@@ -233,32 +245,30 @@ function employeeCoverage(idOrName) {
   // seu código aqui
 
 
+  if (!idOrName) {
+    return getAllEmployeesAndAnimals();
+  }
+  const targetEmployee = getEmployeeByNameOrId(idOrName);
+  const animalList = getAnimalListFromEmployee(targetEmployee);
+  const key = getEmployeeFullName(targetEmployee);
+  return { [key]: animalList };
+
 
   // if (!idOrName) {
-  //   return getAllEmployeesAndAnimals();
+  //   return employees.reduce((accumulator, employee) => {
+  //     const animalList = employee.responsibleFor.map((animalId) => {
+  //       return animals.find((animal) => animalId === animal.id).name;
+  //     });
+  //     const key = `${employee.firstName} ${employee.lastName}`;
+  //     accumulator[key] = animalList;
+  //     return accumulator;
+  //   }, {});
   // }
-  // const targetEmployee = getEmployeeByNameOrId(idOrName);
-  // const animalList = getAnimalListFromEmployee(targetEmployee);
-  // const key = getEmployeeFullName(targetEmployee);
+
+  // const targetEmployee = employees.find((employee) => employee.id === idOrName || employee.firstName === idOrName || employee.lastName === idOrName);
+  // const animalList = targetEmployee.responsibleFor.map((animalId) => animals.find((animal) => animalId === animal.id).name);
+  // const key = `${targetEmployee.firstName} ${targetEmployee.lastName}`;
   // return { [key]: animalList };
-
-
-
-  if (!idOrName) {
-    return employees.reduce((accumulator, employee) => {
-      const animalList = employee.responsibleFor.map((animalId) => {
-        return animals.find((animal) => animalId === animal.id).name;
-      });
-      const key = `${employee.firstName} ${employee.lastName}`;
-      accumulator[key] = animalList;
-      return accumulator;
-    }, {});
-  }
-
-  const targetEmployee = employees.find((employee) => employee.id === idOrName || employee.firstName === idOrName || employee.lastName === idOrName);
-  const animalList = targetEmployee.responsibleFor.map((animalId) => animals.find((animal) => animalId === animal.id).name);
-  const key = `${targetEmployee.firstName} ${targetEmployee.lastName}`;
-  return { [key]: animalList };
 }
 // console.log(employeeCoverage('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
