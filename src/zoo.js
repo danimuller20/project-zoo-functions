9cb0050323d99350c8193f35d8bb073e63d1828c
Pedro Castro <pa.coelho.castro@gmail.com>
2021-02-01 00:30:07 -0300

    Task #8

--- src/zoo.js
+++ src/zoo.js
@@ -69,7 +69,11 @@ function animalCount(species) {
   return allAnimals;
 }
 
-function entryCalculator(entrants) {
+function entryCalculator(entrants = 0) {
+  if(entrants === 0) 0;
+  const {Adult = 0, Child = 0, Senior = 0 } = entrants;
+
+  return (Adult * 49.99) + (Child * 20.99) + (Senior * 24.99)
 }
 
 function animalMap(options) {
