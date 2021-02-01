30e3e303f24be89c27bd9ef21c055537b8e5942f
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-02-01 18:23:13 -0300

    4 requirement - attempt 1

--- src/zoo.js
+++ src/zoo.js
@@ -38,6 +38,11 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  let newEmployee = {};
+  newEmployee = personalInfo;
+  newEmployee.managers = associatedWith.managers;
+  newEmployee.responsibleFor = associatedWith.responsibleFor;
+  return newEmployee;
 }
 
 function isManager(id) {
