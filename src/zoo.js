22235fee30c37fa4754334f7c2ee574fcf371fbe
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2021-02-01 17:28:09 -0300

    Organização Geral

--- src/zoo.js
+++ src/zoo.js
@@ -9,34 +9,25 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');  //recuperando o arquivo data para usar aqui nesse arquivo!
-//const { animals } = data;        //Posso desestruturar o data para pegar o Objeto 'animals' diretamente sem precisar usar: data.animals   para acessa-lo.  
+const data = require('./data');  
 
 function animalsByIds(...ids) {
   if (!ids) return [];
   return data.animals.filter(animal => ids.includes(animal.id));
 }
-
 function animalsOlderThan(animal, age) {
   const animalBySpecie = data.animals.find(({ name }) => name === animal);
   return animalBySpecie.residents.every((value) => value.age >= age);
 }
-
 function employeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
   return data.employees.find( value => value.firstName === employeeName || value.lastName === employeeName);
-};
-  
-
-//
-/* data.employees.find(({ firstName, lastName }) => firstName === employeeName
-      || lastName === employeeName); */
-      
+};      
 function createEmployee(personalInfo, associatedWith) {
   return {
-    ...personalInfo ,
+    ...personalInfo,
     ...associatedWith ,
   };
 };
@@ -67,61 +58,22 @@ function entryCalculator(entrants) {
   return Object.keys(entrants).reduce((ac, cur) => ac + (entrants[cur] * data.prices[cur]), 0);
 };
 
-//*********************************************************
-//*********************************************************
-//9. IMPLEMENTE A FUNÇÃO animalMap
-//A função é responsável pelo mapeamento geográfico das espécies e seus animais, podendo ainda filtrá-los por ordem alfabética e gênero, por exemplo
-//*********************************************************
-//*********************************************************
+
 function animalMap(options) {
-  // seu código aqui
-}
 
-//*********************************************************
-//*********************************************************
-//10. IMPLEMENTE A FUNÇÃO schedule
-//A função é responsável por disponibilizar as informações de horário para uma consulta, que pode querer ter acesso a todo o cronograma da semana ou apenas o cronograma de um dia específico
-//*********************************************************
-//*********************************************************
+}
 function schedule(dayName) {
-  // seu código aqui
-}
 
-//*********************************************************
-//*********************************************************
-//11. IMPLEMENTE A FUNÇÃO oldestFromFirstSpecies
-//A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro
-//*********************************************************
-//*********************************************************
+}
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
 
-//*********************************************************
-//*********************************************************
-//12. IMPLEMENTE A FUNÇÃO increasePrices
-//A função é responsável por aumentar o preço das visitas, com base no valor de aumento recebido no parâmetro, em porcentagem
-//*********************************************************
-//*********************************************************
+}
 function increasePrices(percentage) {
 
-  
-  // seu código aqui
 }
-
-//*********************************************************
-//*********************************************************
-//13. IMPLEMENTE A FUNÇÃO employeeCoverage
-//A função é responsável por consultar as espécies pela qual a pessoa colaborada, recebida no parâmetro através de seu `id`, `firstName` ou `lastName`, é responsável
-//*********************************************************
-//*********************************************************
 function employeeCoverage(idOrName) {
-  // seu código aqui
-}
 
-//*********************************************************
-//*********************************************************
-//*********************************************************
+}
 
 module.exports = {
   entryCalculator,
