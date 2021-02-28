7ad3ebc40adee3b5086075665320cd5d102b5b2c
smylle <jjsilveira007@gmail.com>
2021-02-28 14:29:52 -0300

    Zoo-Functions - #8 - again and again

--- src/zoo.js
+++ src/zoo.js
@@ -74,7 +74,7 @@ function animalCount(species) {
 
 function entryCalculator(entrants = {}) {
   // seu código aqui
-  return Object.entries(entrants).reduce((accumulator, [category, quantity]) => accumulator + (prices[category] * quantity), 0);
+  return Object.entries(entrants).reduce((acc, [categ, quant]) => acc + (prices[categ] * quant), 0);
 }
 
 function animalMap(options) {
