5e53ec735401fd0c648ab7130859bfc563a56cef
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-03-24 23:40:50 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -10,9 +10,10 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-
-function animalsByIds(ids) {
-  // seu código aqui
+const { animals, employees, prices, hours } = data;
+function animalsByIds(...ids) {
+  const foundAnimals = animals.filter(animal => ids.some(id => id === animal.id));
+  return foundAnimals;
 }
 
 function animalsOlderThan(animal, age) {
