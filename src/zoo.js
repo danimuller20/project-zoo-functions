1222f3f0d58df42ef7f429574e9662ca6e7598eb
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-03-25 00:27:54 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -30,6 +30,7 @@ function employeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ... associatedWith };
+  
 }
 
 function isManager(id) {
