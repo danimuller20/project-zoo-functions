a0c0b42d767ee4860792bec8f48af993b11fb7d5
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-03-25 22:16:53 -0300

    Requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -44,11 +44,12 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
-  if (!species)
+  if (!species){
     return animals.reduce((acc, curr) => {
       acc[curr.name] = curr.residents.length;
       return acc;
     }, {});
+  }
   return animals.find(animal => animal.name === species).residents.length;
 }
 
